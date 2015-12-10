f47eb21947b2a30d135088537bb01d452e505b18
Andrew Perry <ajperry@pansapiens.com>
2015-12-10 13:56:27 +1100

    Produce left and right images, both with minimal XMP header data.

--- cardboardcam/controllers/main.py
+++ cardboardcam/controllers/main.py
@@ -17,8 +17,10 @@ from basehash import base62
 from hexahexacontadecimal import hexahexacontadecimal_encode_int as hh_encode_int
 import xxhash
 
+import gc
 from base64 import b64decode
 from libxmp.utils import file_to_dict
+from libxmp import XMPFiles
 
 main = Blueprint('main', __name__)
 
@@ -29,8 +31,10 @@ main = Blueprint('main', __name__)
 #     global upload_folder
 #     upload_folder = setup_state.app.config.get('UPLOAD_FOLDER')
 
+
 def upload_dir():
     return current_app.config.get('UPLOAD_FOLDER', '/tmp')
+
 
 @main.route('/', methods=['GET'])
 @cache.cached(timeout=1000)
@@ -39,10 +43,12 @@ def home():
     filename = None
     return render_template('index.html', form=form, filename=filename)
 
+
 @main.route('/about', methods=['GET'])
 @cache.cached(timeout=1000)
 def about():
     return render_template('about.html')
+
 
 @main.route('/upload', methods=['POST'])
 def upload():
@@ -74,9 +80,10 @@ def upload():
     except:
         abort(500)
 
-    #return jsonify({'redirect': url_for('main.result', img_filename=filename)})
+    # return jsonify({'redirect': url_for('main.result', img_filename=filename)})
     return jsonify({'result_fragment': result(img_id=hash_id), 'img_id': hash_id})
     # return redirect(url_for('main.result', img_filename=filename))
+
 
 def get_hash_id(filepath):
     with open(filepath, 'rb') as file:
@@ -85,20 +92,23 @@ def get_hash_id(filepath):
 
     return hash_str
 
+
 @main.route('/<img_id>', methods=['GET'])
 def result(img_id=None):
     img_filename = img_id + '.jpg'
     upload_folder = upload_dir()
-    input_file = path.join(upload_folder, img_filename)
-    second_image = path.join(upload_folder, get_second_image_name(img_filename))
+    left_img = get_image_name(img_filename, 'left')
+    right_img = get_image_name(img_filename, 'right')
+    left_img_filepath = path.join(upload_folder, left_img)
+    right_img_filepath = path.join(upload_folder, right_img)
     audio_file = path.join(upload_folder, get_audio_file_name(img_filename))
-    if path.isfile(input_file) and path.isfile(second_image) and path.isfile(audio_file):
+    if path.isfile(left_img_filepath) and path.isfile(right_img_filepath) and path.isfile(audio_file):
         pass
     else:
         abort(404)
 
     from PIL import Image
-    image = Image.open(input_file)
+    image = Image.open(left_img_filepath)
     aspect = float(image.size[1]) / float(image.size[0])  # height / width
     thumb_height = str(int(600 * aspect))
 
@@ -112,38 +122,76 @@ def result(img_id=None):
                            # second_img=second_image_url,
                            # audio_file_url=audio_file_url,
                            audio_file=audio_file_url,
-                           left_img=img_filename,
-                           right_img=get_second_image_name(img_filename),
+                           left_img=left_img,
+                           right_img=right_img,
                            thumb_height=thumb_height)
 
-def get_second_image_name(img_filename):
-    return path.splitext(img_filename)[0] + "_righteye.jpg"
+
+def get_image_name(img_filename, eye : str) -> str:
+    return path.splitext(img_filename)[0] + "_%s.jpg" % eye
+
 
 def get_audio_file_name(img_filename):
     return path.splitext(img_filename)[0] + "_audio.mp4"
 
+
 def split_vr_image(img_filename):
-    xmp = file_to_dict(img_filename)
+    XMP_NS_GPHOTOS_IMAGE = u'http://ns.google.com/photos/1.0/image/'
+    XMP_NS_GPHOTOS_AUDIO = u'http://ns.google.com/photos/1.0/audio/'
 
-    audio_b64 = xmp[u'http://ns.google.com/photos/1.0/audio/'][1][1]
+    xmpfile = XMPFiles(file_path=img_filename, open_forupdate=True)
+    xmp = xmpfile.get_xmp()
+    right_image_b64 = xmp.get_property(XMP_NS_GPHOTOS_IMAGE, u'GImage:Data')
+    audio_b64 = xmp.get_property(XMP_NS_GPHOTOS_AUDIO, u'GAudio:Data')
+    xmp.delete_property(XMP_NS_GPHOTOS_IMAGE, u'GImage:Mime')
+    xmp.delete_property(XMP_NS_GPHOTOS_AUDIO, u'GAudio:Mime')
+    xmp.delete_property(XMP_NS_GPHOTOS_IMAGE, u'GImage:Data')
+    xmp.delete_property(XMP_NS_GPHOTOS_AUDIO, u'GAudio:Data')
 
-    afh = open(get_audio_file_name(img_filename), 'wb')
-    afh.write(b64decode(audio_b64))
-    afh.close()
+    # save stripped XMP header to original image
+    if xmpfile.can_put_xmp(xmp):
+        xmpfile.put_xmp(xmp)
+    xmpfile.close_file()
 
-    image_b64 = xmp[u'http://ns.google.com/photos/1.0/image/'][1][1]
+    # save the right image
+    right_img_filename = get_image_name(img_filename, 'right')
+    with open(right_img_filename, 'wb') as fh:
+        fh.write(b64decode(right_image_b64))
+    del right_image_b64
+    # gc.collect()
 
-    ifh = open(get_second_image_name(img_filename), 'wb')
-    ifh.write(b64decode(image_b64))
-    ifh.close()
+    # add stripped XMP header to the right image
+    xmpfile = XMPFiles(file_path=right_img_filename, open_forupdate=True)
+    if xmpfile.can_put_xmp(xmp):
+        xmpfile.put_xmp(xmp)
+    xmpfile.close_file()
+
+    del xmp
+    # gc.collect()
+
+    # save the audio
+    audio_filename = get_audio_file_name(img_filename)
+    with open(audio_filename, 'wb') as fh:
+        fh.write(b64decode(audio_b64))
+    del audio_b64
+    # gc.collect()
+
+    # rename original image
+    left_img_filename = get_image_name(img_filename, 'left')
+    os.rename(img_filename, left_img_filename)
+
+    return (left_img_filename, right_img_filename, audio_filename)
+
 
 @main.errorhandler(404)
 def page_not_found(e):
     return render_error_page(404), 404
 
+
 @main.app_errorhandler(500)
 def page_not_found(e):
     return render_error_page(500), 400
+
 
 def render_error_page(status_code: int):
     return render_template('error_page.html', status_code=status_code)
